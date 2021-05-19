a6fe94f83fe61df1e133d62e56896580e12de08c
Santiago Yanguas <santitoya-2001@outlook.com>
2021-05-19 17:26:25 -0500

    [Feature] Line drawings in 3d

--- src/display.h
+++ src/display.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <math.h>
 
 #define FPS 30
 #define FRAME_TARGET_TIME (1000 / FPS)
@@ -27,6 +28,8 @@ bool initialize_window(void);
 void draw_grid(void);
 void draw_pixel(int x, int y, uint32_t color);
 void draw_rect(int x, int y, int width, int height, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 
 void render_color_buffer(void);
 void clear_color_buffer(uint32_t color);
